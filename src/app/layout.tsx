dd41cde44b86c7ede924e2d1d7f06a082f924aab
roifjcf <roifjcf@google.com>
2025-08-10 01:10:26 +1000

    fix: SEO issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "@/styles/main.scss";
 
 export const metadata: Metadata = {
   title: "holo-fi - play hololive lofi music",
-  description: "A music player that plays relaxing music produced by Hololive.",
+  description: "A music player that plays relaxing lo-fi music produced by Hololive.",
 };
 
 export default function RootLayout({
